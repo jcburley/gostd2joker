2e752c655d600653c3de450e8b2c2762d746ae6d
James Craig Burley <james-github@burleyarch.com>
2018-11-02 22:31:25 -0400

    WiP: Cleaning up testing to fix it

--- test.sh
+++ test.sh
@@ -5,11 +5,11 @@ if [ "$1" = "--on-error" ]; then
     EXIT="$2"
 fi
 
-./gostd2joker -v --source tests/small/net > tests/small/net/TEST.gold 2>&1
-git diff --quiet -u tests/small/net/TEST.gold || { echo >&2 "FAILED: small test"; $EXIT; }
+./gostd2joker -v --source tests/small/net > tests/small/TEST.gold 2>&1
+git diff --quiet -u tests/small/TEST.gold || { echo >&2 "FAILED: small test"; $EXIT; }
 
-./gostd2joker -v --source tests/big/net > tests/big/net/TEST.gold 2>&1
-git diff --quiet -u tests/big/net/TEST.gold || { echo >&2 "FAILED: big test"; $EXIT; }
+./gostd2joker -v --source tests/big/net > tests/big/TEST.gold 2>&1
+git diff --quiet -u tests/big/TEST.gold || { echo >&2 "FAILED: big test"; $EXIT; }
 
 if [ -n "$GOSRC" -a -d "$GOSRC" ]; then
     ./gostd2joker -v --source "$GOSRC" > tests/GOSRC.gold 2>&1
---
+++ tests/small/go/README.md
@@ -0,0 +1,3 @@
+# Placeholder for Empty Go Directory
+
+This is to satisfy one of the requirements for the `--source` option.
