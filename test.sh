0a5e83b5dc7cbc42d68c65d0a465ff806d0a44c1
James Craig Burley <james-github@burleyarch.com>
2018-11-02 22:34:36 -0400

    WiP: Test tests/*/net/*.go files too

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@ if [ "$1" = "--on-error" ]; then
     EXIT="$2"
 fi
 
-./gostd2joker -v --source tests/small/net > tests/small/TEST.gold 2>&1
+./gostd2joker -v --source tests/small > tests/small/TEST.gold 2>&1
 git diff --quiet -u tests/small/TEST.gold || { echo >&2 "FAILED: small test"; $EXIT; }
 
-./gostd2joker -v --source tests/big/net > tests/big/TEST.gold 2>&1
+./gostd2joker -v --source tests/big > tests/big/TEST.gold 2>&1
 git diff --quiet -u tests/big/TEST.gold || { echo >&2 "FAILED: big test"; $EXIT; }
 
 if [ -n "$GOSRC" -a -d "$GOSRC" ]; then
