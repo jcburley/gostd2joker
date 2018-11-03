5b18cf50ad78ceeccb2fe06f3d81b84da3ca14a3
James Craig Burley <james-github@burleyarch.com>
2018-11-03 11:24:53 -0400

    Use existing ../GOSRC file as default for $GOSRC
    This is so I don't have to keep doing `export GOSRC=../GOSRC` or do it globally, which might affect other projects someday.

--- test.sh
+++ test.sh
@@ -11,6 +11,10 @@ git diff --quiet -u tests/small/TEST.gold || { echo >&2 "FAILED: small test"; $E
 ./gostd2joker -v --source tests/big --replace --populate joker/go 2>&1 | grep -v '^Default context:' > tests/big/TEST.gold
 git diff --quiet -u tests/big/TEST.gold || { echo >&2 "FAILED: big test"; $EXIT; }
 
+if [ -z "$GOSRC" -a -e ../GOSRC ]; then
+    GOSRC=../GOSRC
+fi
+
 if [ -n "$GOSRC" -a -d "$GOSRC" ]; then
     ./gostd2joker -v --source "$GOSRC" 2>&1 | grep -v '^Default context:' > tests/GOSRC.gold
     git diff --quiet -u tests/GOSRC.gold || { echo >&2 "FAILED: \$GOSRC test"; $EXIT; }
