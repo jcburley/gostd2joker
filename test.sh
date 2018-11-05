97932b932bbd82ee4d13bea1ce3593b5a2a644e6
James Craig Burley <james-github@burleyarch.com>
2018-11-05 10:14:02 -0500

    Track tests output per-architecture/OS

--- test.sh
+++ test.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+export GOENV="tests/gold/$(go env GOARCH)-$(go env GOOS)"
+mkdir -p "$GOENV"
+
 EXIT="exit 99"
 if [ "$1" = "--on-error" ]; then
     EXIT="$2"
 fi
 
-./gostd2joker -v --source tests/small 2>&1 | grep -v '^Default context:' > tests/small/TEST.gold
-git diff --quiet -u tests/small/TEST.gold || { echo >&2 "FAILED: small test"; $EXIT; }
+./gostd2joker -v --source tests/small 2>&1 | grep -v '^Default context:' > $GOENV/small.gold
+git diff --quiet -u $GOENV/small.gold || { echo >&2 "FAILED: small test"; $EXIT; }
 
-./gostd2joker -v --source tests/big --replace --populate joker/go 2>&1 | grep -v '^Default context:' > tests/big/TEST.gold
-git diff --quiet -u tests/big/TEST.gold || { echo >&2 "FAILED: big test"; $EXIT; }
+./gostd2joker -v --source tests/big --replace --populate joker/go 2>&1 | grep -v '^Default context:' > $GOENV/big.gold
+git diff --quiet -u $GOENV/big.gold || { echo >&2 "FAILED: big test"; $EXIT; }
 
 if [ -z "$GOSRC" -a -e ../GOSRC ]; then
     GOSRC=../GOSRC
 fi
 
 if [ -n "$GOSRC" -a -d "$GOSRC" ]; then
-    ./gostd2joker -v --source "$GOSRC" 2>&1 | grep -v '^Default context:' > tests/GOSRC.gold
-    git diff --quiet -u tests/GOSRC.gold || { echo >&2 "FAILED: \$GOSRC test"; $EXIT; }
+    ./gostd2joker -v --source "$GOSRC" 2>&1 | grep -v '^Default context:' > $GOENV/gosrc.gold
+    git diff --quiet -u $GOENV/gosrc.gold || { echo >&2 "FAILED: \$GOSRC test"; $EXIT; }
 fi
