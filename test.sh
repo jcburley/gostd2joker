6416d1b34f1468ee313bc794e954828b40148a43
James Craig Burley <james-github@burleyarch.com>
2018-11-06 09:37:30 -0500

    test.sh Now Returns Non-zero Status On Failure

--- test.sh
+++ test.sh
@@ -8,11 +8,13 @@ if [ "$1" = "--on-error" ]; then
     EXIT="$2"
 fi
 
+RC=0
+
 ./gostd2joker -v --source tests/small 2>&1 | grep -v '^Default context:' > $GOENV/small.gold
-git diff --quiet -u $GOENV/small.gold || { echo >&2 "FAILED: small test"; $EXIT; }
+git diff --quiet -u $GOENV/small.gold || { echo >&2 "FAILED: small test"; RC=1; $EXIT; }
 
 ./gostd2joker -v --source tests/big --replace --populate joker/go 2>&1 | grep -v '^Default context:' > $GOENV/big.gold
-git diff --quiet -u $GOENV/big.gold || { echo >&2 "FAILED: big test"; $EXIT; }
+git diff --quiet -u $GOENV/big.gold || { echo >&2 "FAILED: big test"; RC=1; $EXIT; }
 
 if [ -z "$GOSRC" -a -e ../GOSRC ]; then
     GOSRC=../GOSRC
@@ -20,5 +22,7 @@ fi
 
 if [ -n "$GOSRC" -a -d "$GOSRC" ]; then
     ./gostd2joker -v --source "$GOSRC" 2>&1 | grep -v '^Default context:' > $GOENV/gosrc.gold
-    git diff --quiet -u $GOENV/gosrc.gold || { echo >&2 "FAILED: \$GOSRC test"; $EXIT; }
+    git diff --quiet -u $GOENV/gosrc.gold || { echo >&2 "FAILED: \$GOSRC test"; RC=1; $EXIT; }
 fi
+
+exit $RC
