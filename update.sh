58c97362e5433a23823f8ed0f48f4fbf8105b892
James Craig Burley <james-github@burleyarch.com>
2018-11-07 10:45:22 -0500

    Create handy script for updating other machines' test results

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+GOENV="$(go env GOARCH)-$(go env GOOS)"
+
+git pull && go clean && go vet && go build && ./test.sh --on-error : && echo "No changes to $GOENV test results." && exit 0
+
+read -p "Accept and update $GOENV test results? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git commit -a -m "Update $GOENV tests" && git push
+fi
