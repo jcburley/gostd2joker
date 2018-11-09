ea3f687cdb3d559870d5a824651502f0d918337b
James Craig Burley <james-github@burleyarch.com>
2018-11-09 18:50:04 -0500

    Add generate files to 'big' test, hoping they're arch/os-independent

--- test.sh
+++ test.sh
@@ -13,6 +13,8 @@ RC=0
 ./gostd2joker -v --go tests/small 2>&1 | grep -v '^Default context:' > $GOENV/small.gold
 git diff --quiet -u $GOENV/small.gold || { echo >&2 "FAILED: small test"; RC=1; $EXIT; }
 
+rm -fr tests/joker
+cp -pr tests/joker.orig tests/joker
 ./gostd2joker -v --go tests/big --replace --joker tests/joker 2>&1 | grep -v '^Default context:' > $GOENV/big.gold
 git diff --quiet -u $GOENV/big.gold || { echo >&2 "FAILED: big test"; RC=1; $EXIT; }
 
